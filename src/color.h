6687fd67e7c41bbc4d3031c8c48306586eb56a3a
Ivan Velkov <sasmose@yahoo.com>
2015-12-13 20:25:04 +0200

    Sad panda solution

--- src/color.h
+++ src/color.h
@@ -33,6 +33,53 @@ inline unsigned convertTo8bit(float x)
 	return nearestInt(x * 255.0f);
 }
 
+//  http://martin.ankerl.com/2012/01/25/optimized-approximative-pow-in-c-and-cpp/
+inline float fastPow(float a, float b) {
+  union {
+    double d;
+    int x[2];
+  } u = { a };
+  u.x[1] = (int)(b * (u.x[1] - 1072632447) + 1072632447);
+  u.x[0] = 0;
+  return u.d;
+}
+
+inline double fastPrecisePow(double a, double b) {
+  int e = (int) b;
+  union {
+    double d;
+    int x[2];
+  } u = { a };
+  u.x[1] = (int)((b - e) * (u.x[1] - 1072632447) + 1072632447);
+  u.x[0] = 0;
+
+  double r = 1.0;
+  while (e) {
+    if (e & 1) {
+      r *= a;
+    }
+    a *= a;
+    e >>= 1;
+  }
+
+  return r * u.d;
+}
+
+inline unsigned convertTo8bit_sRGB(float x)
+{
+    float a = 0.055f;
+    float delta=0.0031308f;
+
+    if(x<=delta)
+        x*=12.92f;
+    else
+        x = (1 + a) * powf(x, 1/2.4) - a; // fastPow and fastPrecisePow
+
+	if (x < 0) x = 0;
+	if (x > 1) x = 1;
+	return nearestInt(x * 255.0f);
+}
+
 /// Represents a color, using floatingpoint components in [0..1]
 struct Color {
 	// a union, that allows us to refer to the channels by name (::r, ::g, ::b),
@@ -57,9 +104,9 @@ struct Color {
 	/// the blue channel occupying the least-significant byte
 	unsigned toRGB32(int redShift = 16, int greenShift = 8, int blueShift = 0)
 	{
-		unsigned ir = convertTo8bit(r);
-		unsigned ig = convertTo8bit(g);
-		unsigned ib = convertTo8bit(b);
+		unsigned ir = convertTo8bit_sRGB(r);
+		unsigned ig = convertTo8bit_sRGB(g);
+		unsigned ib = convertTo8bit_sRGB(b);
 		return (ib << blueShift) | (ig << greenShift) | (ir << redShift);
 	}
 	/// make black
@@ -105,12 +152,12 @@ struct Color {
 		g /= divider;
 		b /= divider;
 	}
-	
+
 	inline const float& operator[] (int index) const
 	{
 		return components[index];
 	}
-	
+
 	inline float& operator[] (int index)
 	{
 		return components[index];
